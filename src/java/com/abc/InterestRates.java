277740c7182d413320ea6c6547d0478bd4087ab6
Charishma-G <gcharishma30@gmail.com>
2015-11-09 10:11:10 -0500

    Create InterestRates.java

---
+++ src/java/com/abc/InterestRates.java
@@ -0,0 +1,12 @@
+package com.abc;
+
+public interface InterestRates {
+	 public double SAVINGS_THRESHOLD = 1000;
+	 public double MAXI_SAVINGS_THRESHOLD = 1000;
+	 public double CHECKING_FLAT_RATE = 0.001;
+	 public double SAVINGS_RATE = 0.001;
+	 public double SAVINGS_RATE_ABOVE_THRESHOLD = 0.002;
+	 public double MAXI_SAVINGS_RATE_BELOW_THRESHOLD = 0.02;
+	 public double MAXI_SAVINGS_RATE_AT_THRESHOLD = 0.05;
+	 public double MAXI_SAVINGS_RATE_ABOVE_THRESHOLD = 0.1;
+}
