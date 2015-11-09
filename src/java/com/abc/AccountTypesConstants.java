f6294f66eb9c2744a85da3e108882a1ac48113ee
Charishma-G <gcharishma30@gmail.com>
2015-11-09 10:10:32 -0500

    Create AccountTypesConstants.java

---
+++ src/java/com/abc/AccountTypesConstants.java
@@ -0,0 +1,7 @@
+package com.abc;
+
+public interface AccountTypesConstants {
+    public static final int CHECKING = 0;
+    public static final int SAVINGS = 1;
+    public static final int MAXI_SAVINGS = 2;
+}
