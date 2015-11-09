b1bc7f60f2d93bee5be16f6e7374ea785c057f64
Charishma-G <gcharishma30@gmail.com>
2015-11-09 10:06:48 -0500

    Update DateProvider.java

--- src/java/com/abc/DateProvider.java
+++ src/java/com/abc/DateProvider.java
@@ -2,6 +2,7 @@ package com.abc;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class DateProvider {
     private static DateProvider instance = null;
@@ -14,5 +15,15 @@ public class DateProvider {
 
     public Date now() {
         return Calendar.getInstance().getTime();
+    }
+    
+   
+    public static long getDateDiffernce(Date oldDate, Date newDate, TimeUnit timeUnit) {
+        if(newDate.getTime()>oldDate.getTime()){
+    	long diffInMillies = newDate.getTime() - oldDate.getTime();
+        return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
+        }else{
+        	return -1;
+        }
     }
 }
